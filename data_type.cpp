8a78dab52835df1e7f8466f87408de4180324f4b
Dit <145139482+Elite011@users.noreply.github.com>
2024-07-27 07:56:42 +0530

    Basic

---
+++ data_type.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){
+    int a = 5;
+    int b = 6;
+    std:: cout<<"hello world! Here are two variables "<<a<< " and " <<b;
+    return 0;
+}
---
+++ user_input.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+    int a,b;
+    std::cout<< "Enter you first number:\n";
+    std:: cin>>a;
+    std::cout<< "Enter you first number:\n";
+    std:: cin>>b;
+    std:: cout<< "The sum of a and b is:\n"<< a+b;
+    return 0 ;
+
+}
